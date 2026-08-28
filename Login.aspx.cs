a8a3a2d564dfc20499758c5dfcaadfbba9a78c6b
shaimor9000-bit <shaimor9000@gmail.com>
2026-08-29 02:53:49 +0300

    updating login

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -13,50 +13,30 @@ namespace Web2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if(IsPostBack==false)// כניסה ראשונה לעמוד
-            {
-                TxtUser.Text = "bla bla";
-                
-                int a;
-                a = 4;
-                string b;
-                User u1;// כרגע משתנה הייחוס מכיל נאל
-                u1= new User(100,"ylapidot@gmail.com","123456","Yaron Lapidot");// יצירת אובייקט מסוג משתמש בשם יו אחד
 
-                // אנחנו מפעילים את פונקציית הבנאי בעת יצירת אובייקט
-                // User() - זו הפעלה של הבנאי של המחלקה יוזר
-                // במידה ולא הוגדר במחלקה בנאי, המערכת מייצרת מאחורי הקלעים בנאי ברירת מחדל עבורנו
-                //? מה יש בתוך התכונות של יו אחד לאחר הפקודה
-                //אפסים ונאלים מחרוזת ריקה, כלום, הכול מאופס
-                // מה נעשה, במידה ואנחנו מעונייםנים בעת יצירת האוביקט לתת לו ערכים משלנו במקום האפסים והנאלים?
-                // תשובה: ניצור פונקציה שתאתחל את השדות שלו, לפונקציה זו קוראים בנאי
-                u1.UserName = "aaa";
-                u1.Pass = "bbb";// הכנסה לתוך השדות ערכים
-                User u2 = new User("aaa","bbb");
-                u1.ChkLogin();
-                BLL.User.ChkLoginExternal("","");// הפעלת פעולה 
-
-                BLL.User.Max(3, 5);
-
-            }
-          
         }
 
         protected void BtnLogin_Click(object sender, EventArgs e)
         {
-            string Email=TxtUser.Text;
-            string Pass=TxtPass.Text;
-            User user = new User(Email,Pass);// יצירת אובייקט מסוג משתמש
-            if(user.ChkLogin())// במידה והשם והסיסמה תקינים, התחבר למערכת
-            {
-                Session["Login"]=user;//שמירת אוביקט המשתמש בתוך משתנה מסוג סשן
-                Response.Redirect("default.aspx");// מעבר לעמוד הבית
-            }
-            else// השם והסיסמה אינם תקינים, הצג הודעה מתאימה
-            {
-                LtlMsg.Text = "<div class='badge badge-error' >שם / סיסמה אינם נכונים</div>";
-            }
+            var phone = TxtUser.Text.Trim();
+            var pass = TxtPass.Text;
 
+            User loggedInUser;
+            var result = User.Login(phone, pass, out loggedInUser);// בדיקה מול מסד הנתונים
+
+            switch (result)
+            {
+                case LoginResult.Success:
+                    Session["Login"] = loggedInUser;// שמירת אוביקט המשתמש בתוך משתנה מסוג סשן
+                    Response.Redirect("default.aspx");// מעבר לעמוד הבית
+                    break;
+                case LoginResult.AlreadyConnected:
+                    LtlMsg.Text = "<div class='badge badge-error'>המשתמש הזה כבר מחובר ממקום אחר, התנתק שם קודם</div>";
+                    break;
+                default:
+                    LtlMsg.Text = "<div class='badge badge-error'>טלפון / סיסמה אינם נכונים</div>";
+                    break;
+            }
         }
 
         protected void btnTmp_Click(object sender, EventArgs e)

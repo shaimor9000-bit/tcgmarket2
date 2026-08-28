2816ed6710d7b8dfeb7f749d0d616d88cdde8ba8
shaimor9000-bit <shaimor9000@gmail.com>
2026-08-29 02:48:04 +0300

    some fixing

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,98 +1,75 @@
-﻿using DAL;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Driver;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using BLL;
+using DAL;
 
-namespace BLL
+namespace Web2
 {
-    public enum RegisterResult
+    public class Global : System.Web.HttpApplication
     {
-        Success,
-        DuplicateUserName,
-        DuplicateEmail,
-        DuplicatePhone
-    }
 
-    public enum LoginResult
-    {
-        Success,
-        WrongPassword,
-        NotFound,
-        AlreadyConnected
-    }
-
-    [BsonIgnoreExtraElements]
-    public class User
-    {
-        [BsonId]
-        public ObjectId Id { get; set; }
-        public string UserName { get; set; }// שם משתמש
-        public string Email { get; set; }// מייל
-        public string Phone { get; set; }// טלפון, זה גם השדה שדרכו מתחברים למערכת
-        public string FullName { get; set; }// שם מלא
-        public string Address { get; set; }// כתובת
-        public int BirthYear { get; set; }// שנת לידה
-        public int CityId { get; set; }// קוד עיר
-        public string PasswordHash { get; set; }
-        public string PasswordSalt { get; set; }
-        public bool IsLoggedIn { get; set; }// דגל שמראה אם המשתמש כבר מחובר במקום אחר
-
-        [BsonIgnore]
-        public string Pass { get; set; }// סיסמה בטקסט רגיל, רק זמנית בזמן הרשמה/התחברות, לא נשמר במונגו
-
-        static IMongoCollection<User> Users
+        protected void Application_Start(object sender, EventArgs e)// אירוע טעינה של האפליקציה
         {
-            get { return MongoHelper.GetDatabase().GetCollection<User>("Users"); }
+            try
+            {
+                MongoHelper.EnsureIndexes();// יוצר את האינדקסים הייחודיים במונגו (שם משתמש, מייל, טלפון)
+            }
+            catch (Exception ex)
+            {
+                // אם המונגו לא רץ כרגע, לא נרצה שהאתר יקרוס, רק שנדע שהייתה בעיה
+                System.Diagnostics.Debug.WriteLine("Mongo connection failed: " + ex.Message);
+            }
+
+            // אנחנו מדמים שליפה של מרשימת המוצרים מתוך בסיס הנתונים
+            List<Product> lst = new List<Product>();// יצירת רשימה של מוצרים
+            Product p = new Product();// יצירת אובייקט סוג מוצר
+            p.PName = "aaa";
+            lst.Add(p);// הוספת האובייקט לרשימה
+            p = new Product();// יצירת אובייקט סוג מוצר
+            p.PName = "bbb";
+            lst.Add(p);// הוספת האובייקט לרשימה
+            p = new Product();// יצירת אובייקט סוג מוצר
+            p.PName = "ccc";
+            lst.Add(p);// הוספת האובייקט לרשימה
+            p = new Product();// יצירת אובייקט סוג מוצר
+            p.PName = "zzz";
+            lst.Add(p);// הוספת האובייקט לרשימה
+            Application["Prods"] = lst;// שמירה רשימת המוצרים בתוך אובייקט מסוג אפליקיישן
+
         }
 
-        public RegisterResult Register()
+        protected void Session_Start(object sender, EventArgs e)
         {
-            if (Users.Find(u => u.UserName == UserName).Any())
-                return RegisterResult.DuplicateUserName;
-            if (Users.Find(u => u.Email == Email).Any())
-                return RegisterResult.DuplicateEmail;
-            if (Users.Find(u => u.Phone == Phone).Any())
-                return RegisterResult.DuplicatePhone;
 
-            var hash = "";
-            var salt = "";
-            PasswordHelper.CreateHash(Pass, out hash, out salt);
-            PasswordHash = hash;
-            PasswordSalt = salt;
-            IsLoggedIn = false;
-
-            Users.InsertOne(this);
-            return RegisterResult.Success;
         }
 
-        public static LoginResult Login(string phone, string pass, out User user)
+        protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            user = Users.Find(u => u.Phone == phone).FirstOrDefault();
 
-            if (user == null)
-                return LoginResult.NotFound;
-
-            if (!PasswordHelper.Verify(pass, user.PasswordHash, user.PasswordSalt))
-                return LoginResult.WrongPassword;
-
-            if (user.IsLoggedIn)
-                return LoginResult.AlreadyConnected;
-
-            var filter = Builders<User>.Filter.Eq(u => u.Id, user.Id);
-            var update = Builders<User>.Update.Set(u => u.IsLoggedIn, true);
-            Users.UpdateOne(filter, update);
-            user.IsLoggedIn = true;
-
-            return LoginResult.Success;
         }
 
-        public static void Logout(ObjectId userId)
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
-            var filter = Builders<User>.Filter.Eq(u => u.Id, userId);
-            var update = Builders<User>.Update.Set(u => u.IsLoggedIn, false);
-            Users.UpdateOne(filter, update);
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
         }
     }
 }
